7bbffdc4e6b83ecfca87353d83168396cc3d1da1
purplest <purplest@qq.com>
2015-05-17 17:05:33 +0800

    add bean

---
+++ src/main/java/com/mpp/model/entity/applyChangeStateBean.java
@@ -0,0 +1,34 @@
+package com.mpp.model.entity;
+
+/**
+ * Created by xiang.xu on 2015/5/17.
+ */
+public class ApplyChangeStateBean {
+    private int applyId;
+    private int state;
+    private String msg;
+
+    public int getApplyId() {
+        return applyId;
+    }
+
+    public void setApplyId(final int applyId) {
+        this.applyId = applyId;
+    }
+
+    public int getState() {
+        return state;
+    }
+
+    public void setState(final int state) {
+        this.state = state;
+    }
+
+    public String getMsg() {
+        return msg;
+    }
+
+    public void setMsg(final String msg) {
+        this.msg = msg;
+    }
+}
